a5493a1751c2847d7099ea96c05378bbcb95a29e
Hiep <danghiep0122@gmail.com>
2022-04-05 21:49:41 +0700

    them file css

--- dom.js
+++ dom.js
@@ -1,7 +1,20 @@
 
-document.write('Hello 👋🏫👩‍💻');
+// let getHeading = document.querySelector('h2')
+// getHeading.setAttribute('class', 'heading')
 
-var headingNode = document.getElementById('heading')
-console.log({
-    element: headingNode
-});
+// let getBox = document.querySelector('div');
+// getBox.setAttribute('class','heading'); 
+// console.log(getBox)
+// // getBox.innerHTML = '<h5>Test</h5>';
+
+let textNote1 = document.getElementsByTagName('div')[0].firstChild
+let h1Element = document.getElementsByTagName('div')[0].firstChild.nextSibling
+let textNote2 = document.getElementsByTagName('div')[0].lastChild
+
+
+
+console.log(textNote1)
+console.log(textNote2)
+console.log(h1Element)
+console.log(textNotex)
+
