1ef4988bd9a5fcfc489d493b1240e48b9da6e5c8
D.Hiep <vostro1250@gmail.com>
2022-04-15 22:51:25 +0700

    bai tap Tab click

---
+++ app.js
@@ -0,0 +1,15 @@
+// const arr = ['PES', 'eFootBall', 'FIFA', 'FO4']
+
+// let [a, b, c, d] = arr
+
+// console.log(a, c)
+
+// const fieldName = 'game';
+// const fieldPrice = 'price';
+
+// let obj = {
+//     [fieldName]: 'Pes eFootBall',
+//     [fieldPrice]: 1000
+// }
+
+// console.log(obj)
--- dom.js
+++ dom.js
@@ -7,14 +7,14 @@
 // console.log(getBox)
 // // getBox.innerHTML = '<h5>Test</h5>';
 
-let textNote1 = document.getElementsByTagName('div')[0].firstChild
-let h1Element = document.getElementsByTagName('div')[0].firstChild.nextSibling
-let textNote2 = document.getElementsByTagName('div')[0].lastChild
+// let textNote1 = document.getElementsByTagName('div')[0].firstChild
+// let h1Element = document.getElementsByTagName('div')[0].firstChild.nextSibling
+// let textNote2 = document.getElementsByTagName('div')[0].lastChild
 
 
 
-console.log(textNote1)
-console.log(textNote2)
-console.log(h1Element)
+// console.log(textNote1)
+// console.log(textNote2)
+// console.log(h1Element)
 // console.log(textNote)
 
