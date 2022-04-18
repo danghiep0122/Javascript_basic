b6ea0de9466ea4405934694f9844885359c885bf
D.Hiep <vostro1250@gmail.com>
2022-04-18 23:26:11 +0700

    xong bai tap

--- app.js
+++ app.js
@@ -14,14 +14,29 @@
 
 // console.log(obj)
 
-const $=document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$('.tab-item')
 const panes = $$('.pane-item')
 
+const tabActive = $('.tab-item.active')
+const line = $('.tabs .line')
+
+
+
+console.log([tabActive]);
+
 tabs.forEach((tab, index) => {
+    const pane = panes[index]
     tab.onclick = function () {
+        $('.tab-item.active').classList.remove('active');
+        $('.pane-item.active').classList.remove('active');
+
         this.classList.add('active');
+        pane.classList.add('active');
+        
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';    
     }
 });
